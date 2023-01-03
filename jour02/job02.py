795df10f06e371ade8cd74aff0f7ccf66d2bf5ba
Zaynab <115147390+zaynab-kaf@users.noreply.github.com>
2023-01-04 00:21:57 +0100

    Add files via upload

---
+++ jour02/job02.py
@@ -0,0 +1,8 @@
+def My_print_name (name):
+
+    prenom = name
+
+    print ( prenom )
+
+My_print_name ( "Zaynab" )
+My_print_name ( "Zayn" )
---
+++ jour02/job03.py
@@ -0,0 +1,10 @@
+def add(num1,num2):
+   
+   result=num1+num2
+   
+   print(result)
+
+add(12,15)
+
+
+
